cb38c21a06053960e48a09c95dc2ec37850dc92f
Georgia <georgialloydphoto@gmail.com>
2025-06-06 18:57:25 -0500

    Fix minor formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { useState } from "react";
 
-
 import "./App.css";
-
-
 import Header from "./components/Header/Header";
 import LandingPage from "./components/LandingPage/LandingPage";
 import Main from "./components/Main/Main";
