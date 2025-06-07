51d5b44fd341c20a88b2e97100963ccd4dbcb522
Georgia <georgialloydphoto@gmail.com>
2025-06-07 11:32:26 -0500

    Fix modal centering

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-// georgia branch
+
 import "./App.css";
 import Header from "./components/Header/Header";
 import LandingPage from "./components/LandingPage/LandingPage";
