a20f47b256d1153a415974f391de3d10f0551c49
Georgia <georgialloydphoto@gmail.com>
2025-06-07 14:51:35 -0500

    Add header links

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,34 +1,25 @@
+import { Link } from "react-router-dom";
 import "./Header.css";
 import logo from "../../assets/travellogo.png";
 import globe from "../../assets/globewatercolor.jpg";
 import avatar from "../../assets/avatarPlaceholder.jpg";
 
 function Header() {
-  // logo on the left
-  // login button
-  //add new log
   return (
     <header className="header">
       <div className="header__container">
-        <img className="header__logo" src={logo} alt="Memory Map Logo" />
+        <Link to="/" className="header__link">
+          <img className="header__logo" src={logo} alt="Memory Map Logo" />
+        </Link>
         <img className="header__image" src={globe} alt="Globe Water Color" />
         <button className="header__login-button">Login</button>
         <div className="header__user-container">
-          {/* Putting the login button in this container 
-              - if the user is logged in we will render the add-log-button as well
-              as the user info and remove the login button
-              
-              Will add the conditional statements later. Can style as normal,
-              but will show all three items until I fix the logic. After logic
-              only Login button will show
-              
-              upon successful login, login-button dissapears and add-log-button
-              and the user info will be visible and need a flexbox*/}
-
           <button className="header__add-log-button">New log</button>
           <div className="header__user-details">
-            <img className="header__avatar" src={avatar} />
-            <p className="header__username">Josh Johnson</p>
+            <Link to="/profile" className="header__link">
+              <img className="header__avatar" src={avatar} />
+              <p className="header__username">Josh Johnson</p>
+            </Link>
           </div>
         </div>
       </div>
