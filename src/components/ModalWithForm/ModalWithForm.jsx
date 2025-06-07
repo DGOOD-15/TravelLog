f95aa0abf056c6b1b0fd428c73207280de606b9e
Jaimie Bowen <jraebowen@gmail.com>
2025-06-07 05:44:44 -0700

    style: add styling to modals, update header buttons to match

--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,5 +1,6 @@
 import "./ModalWithForm.css";
 import useModalClose from "../../hooks/useModalClose";
+import closeButton from "../../assets/closeButton.png";
 
 function ModalWithForm({
   children,
@@ -16,8 +17,13 @@ function ModalWithForm({
     <div className={`modal ${isOpen ? "modal_opened" : ""}`}>
       <form className="modal__form" onSubmit={onSubmit}>
         <h2 className="modal__title">{title}</h2>
-        <button onClick={onClose} type="button" className="modal__close" />
-        {children}
+        <button
+          onClick={onClose}
+          type="button"
+          className="modal__close"
+          src={closeButton}
+        />
+        <div className="modal__form-fields">{children}</div>
         <div className="modal__submit-container">
           <button type="submit" className="modal__submit">
             {buttonText}
