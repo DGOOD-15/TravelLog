1cb07b928177dce7021de6403facd0ba039f6467
Georgia <georgialloydphoto@gmail.com>
2025-06-08 09:07:35 -0500

    working modals

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,7 +2,7 @@ import "./Profile.css";
 import LogsSection from "../LogsSection/LogsSection";
 import Sidebar from "../Sidebar/Sidebar";
 import EditProfileModal from "../EditProfileModal/EditProfileModal";
-
+// working modals
 function Profile({
   onAddLogClick,
   onLogClick
